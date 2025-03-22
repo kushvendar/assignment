6e34637a504d6958d75714f8457230eba503f2a4
Kush <jangidkush1234@gmail.com>
2025-03-22 22:29:49 +0530

    logic done

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './App.css'
+import Matrix from './components/Matrix'
+function App() {
+  return (
+  //   <div className='items-center justify-center '>
+  //     <div className='flex flex-row'>
+  //       <div className='flex flex-col w-[20px] items-center border-[2px] '>1</div>
+  //       <div className='flex flex-col w-[20px] items-center border-[2px] '>2</div>
+  //       <div className='flex flex-col w-[20px] items-center border-[2px] '>3</div>
+  //     </div>
+  //     <div className='flex flex-row'>
+  //       <div className='flex flex-col w-[20px] items-center border-[2px] '>4</div>
+  //       <div className='flex flex-col w-[20px] items-center border-[2px] '>5</div>
+  //       <div className='flex flex-col w-[20px] items-center border-[2px] '>6</div>
+  //     </div>
+  //     <div className='flex flex-row'>
+  //       <div className='flex flex-col w-[20px] items-center border-[2px] '>7</div>
+  //       <div className='flex flex-col w-[20px] items-center border-[2px] '>8</div>
+  //       <div className='flex flex-col w-[20px] items-center border-[2px] '>9</div>
+  //     </div>
+  //   </div>
+  // 
+  <div><Matrix/></div>
+  )
+}
+
+export default App
