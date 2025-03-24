3f6fe3a6cca6bab43c900fa4791d3919e51749bc
Kush <jangidkush1234@gmail.com>
2025-03-24 23:04:31 +0530

    checking algo

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,25 @@ import Matrix from './components/Matrix'
 import Logic from './components/Logic'
 function App() {
   return (
-  //   <div className='items-center justify-center '>
-  //     <div className='flex flex-row'>
-  //       <div className='flex flex-col w-[20px] items-center border-[2px] '>1</div>
-  //       <div className='flex flex-col w-[20px] items-center border-[2px] '>2</div>
-  //       <div className='flex flex-col w-[20px] items-center border-[2px] '>3</div>
-  //     </div>
-  //     <div className='flex flex-row'>
-  //       <div className='flex flex-col w-[20px] items-center border-[2px] '>4</div>
-  //       <div className='flex flex-col w-[20px] items-center border-[2px] '>5</div>
-  //       <div className='flex flex-col w-[20px] items-center border-[2px] '>6</div>
-  //     </div>
-  //     <div className='flex flex-row'>
-  //       <div className='flex flex-col w-[20px] items-center border-[2px] '>7</div>
-  //       <div className='flex flex-col w-[20px] items-center border-[2px] '>8</div>
-  //       <div className='flex flex-col w-[20px] items-center border-[2px] '>9</div>
-  //     </div>
-  //   </div>
-  // 
-    <div><Logic/></div>
+    <div className='items-center justify-center '>
+      <div className='flex flex-row'>
+        <div className='flex flex-col w-[20px] items-center border-[2px] '>1</div>
+        <div className='flex flex-col w-[20px] items-center border-[2px] '>2</div>
+        <div className='flex flex-col w-[20px] items-center border-[2px] '>3</div>
+      </div>
+      <div className='flex flex-row'>
+        <div className='flex flex-col w-[20px] items-center border-[2px] '>4</div>
+        <div className='flex flex-col w-[20px] items-center border-[2px] '>5</div>
+        <div className='flex flex-col w-[20px] items-center border-[2px] '>6</div>
+      </div>
+      <div className='flex flex-row'>
+        <div className='flex flex-col w-[20px] items-center border-[2px] '>7</div>
+        <div className='flex flex-col w-[20px] items-center border-[2px] '>8</div>
+        <div className='flex flex-col w-[20px] items-center border-[2px] '>9</div>
+      </div>
+    </div>
+   
+   
   )
 }
 
