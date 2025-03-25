054d7bf722695c4375f8dc3d065a23ab9a021abe
Kush <jangidkush1234@gmail.com>
2025-03-25 16:08:55 +0530

    logic done working smoothly

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
-import Matrix from './components/Matrix'
-import Logic from './components/Logic'
 function App() {
+  // created an 2-d array to store white color
+  const [box,setBox]=useState(Array(9).fill('white'));
+
+  // created an array to store boxes clicked
+  const[order,setOrder]=useState([])
+
+  // on clicking the box marking box green and saving the index in order array
+  const handleClick=(idx)=>{
+    if(box[idx]==='white'){
+      const newbox=[...box]
+      newbox[idx]='green'
+      setBox(newbox)
+      setOrder([...order,idx])
+    } 
+    if(idx===8){
+      // function to create a new set of array with color orange sequence
+      clickedLastBox([...order,idx])
+    }
+    
+  }
+
+  // when clicked on last box taking the idx from the order array and changing them to orange and added a time delay to show to effect 
+  const clickedLastBox=(order)=>{
+      order.forEach((idx,i) => {
+        setTimeout(()=>{
+          setBox((oldBox)=>{
+            const newBox=[...oldBox]
+            newBox[idx]='orange';
+            return newBox
+          })
+        },i*500)
+      });
+  }
+
   return (
-    <div className='items-center justify-center '>
-      <div className='flex flex-row'>
-        <div className='flex flex-col w-[20px] items-center border-[2px] '>1</div>
-        <div className='flex flex-col w-[20px] items-center border-[2px] '>2</div>
-        <div className='flex flex-col w-[20px] items-center border-[2px] '>3</div>
-      </div>
-      <div className='flex flex-row'>
-        <div className='flex flex-col w-[20px] items-center border-[2px] '>4</div>
-        <div className='flex flex-col w-[20px] items-center border-[2px] '>5</div>
-        <div className='flex flex-col w-[20px] items-center border-[2px] '>6</div>
-      </div>
-      <div className='flex flex-row'>
-        <div className='flex flex-col w-[20px] items-center border-[2px] '>7</div>
-        <div className='flex flex-col w-[20px] items-center border-[2px] '>8</div>
-        <div className='flex flex-col w-[20px] items-center border-[2px] '>9</div>
-      </div>
+    // using grid layout as it suited good and now creating a div for every element with the color stored in box array in addition to tailwind using sytle attritute to bind the logic
+    <div className='grid grid-cols-3 gap-2 w-40 mx-auto mt-50 '>
+       {box.map((color,idx) => (
+      <div
+        key={idx}
+        className="w-12 h-12 border cursor-pointer"
+        style={{ backgroundColor: color }}
+        onClick={() => handleClick(idx)}
+      />
+    ))}
     </div>
-   
-   
   )
 }
 
